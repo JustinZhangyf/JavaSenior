17611289bc5e1d93d2f72e45ad996985f21c71aa
justinzhang <justinzhangyf@gmail.com>
2021-01-08 00:11:08 +0800

    测试

---
+++ day04/src/com/atguigu/exer/StringDemo.java
@@ -0,0 +1,30 @@
+package com.atguigu.exer;
+
+/**
+ * 1.模拟一个trim方法，去除字符串两端的空格。
+ * <p>
+ * 2.将一个字符串进行反转。将字符串中指定部分进行反转。比如将“abcdefg”反转为”abfedcg”
+ * <p>
+ * 3.获取一个字符串在另一个字符串中出现的次数。
+ * 比如：获取“ab”在 “cdabkkcadkabkebfkabkskab”中出现的次数
+ * <p>
+ * 4.获取两个字符串中最大相同子串。比如：
+ * str1 = "abcwerthelloyuiodef“;str2 = "cvhellobnm"//10
+ * 提示：将短的那个串进行长度依次递减的子串与较长的串比较。
+ * <p>
+ * 5.对字符串中字符进行自然顺序排序。"abcwerthelloyuiodef"
+ * 提示：
+ * 1）字符串变成字符数组。
+ * 2）对数组排序，选择，冒泡，Arrays.sort(str.toCharArray());
+ * 3）将排序后的数组变成字符串。
+ */
+public class StringDemo {
+    // 2.将一个字符串进行反转。将字符串中指定部分进行反转。比如将“abcdefg”反转为”abfedcg”
+    public String reverse(String str, int startIndex, int endIndex){
+
+        char[] arr = str.toCharArray();
+        return null;
+    }
+
+
+}
