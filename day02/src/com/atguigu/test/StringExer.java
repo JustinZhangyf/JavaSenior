93e458cd0be2b176df0ba1da731d2ad0ddebd90e
justinzhang <justinzhangyf@gmail.com>
2021-01-08 00:09:08 +0800

    新建day04module

--- day02/src/com/atguigu/test/StringExer.java
+++ day02/src/com/atguigu/test/StringExer.java
@@ -91,6 +91,7 @@ public class StringExer {
     //4.获取两个字符串中最大相同子串。比如：
     //str1 = "abcwerthelloyuiodef“;str2 = "cvhellobnm"//10
     //提示：将短的那个串进行长度依次递减的子串与较长的串比较。
+    @Test
     public void test4(){
 
     }
