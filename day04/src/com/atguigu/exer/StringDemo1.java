4c28dc3495fdc0630f28bde1471ee2e458b30d41
Justin.Zhang <justinzhangyf@gmail.com>
2021-01-09 13:49:44 +0800

    StringCount Demo

---
+++ day04/src/com/atguigu/exer/StringDemo1.java
@@ -0,0 +1,23 @@
+package com.atguigu.exer;
+
+/**
+ * @author Justin.Zhang
+ * @Description
+ * @date Created in 2021-01-1/9/2021-1:00 PM
+ */
+/*
+ * 3.获取一个字符串在另一个字符串中出现的次数。
+ * 比如：获取“ab”在 “cdabkkcadkabkebfkabkskab”中出现的次数
+ */
+public class StringDemo1 {
+    /**
+     *
+     * @param mainStr
+     * @param subStr
+     * @return
+     */
+    public int getStrCount(String mainStr, String subStr){
+        return 0;
+    }
+
+}
