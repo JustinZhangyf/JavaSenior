161b50abb82cdfbc367d64443c418a81140427e2
Justin.Zhang <justinzhangyf@gmail.com>
2021-01-09 13:01:40 +0800

    reverseString Demo

--- day04/src/com/atguigu/exer/StringDemo.java
+++ day04/src/com/atguigu/exer/StringDemo.java
@@ -1,5 +1,8 @@
 package com.atguigu.exer;
 
+
+import org.junit.Test;
+
 /**
  * 1.模拟一个trim方法，去除字符串两端的空格。
  * <p>
@@ -20,11 +23,61 @@ package com.atguigu.exer;
  */
 public class StringDemo {
     // 2.将一个字符串进行反转。将字符串中指定部分进行反转。比如将“abcdefg”反转为”abfedcg”
-    public String reverse(String str, int startIndex, int endIndex){
 
-        char[] arr = str.toCharArray();
+
+    //方法1
+    public String reverse(String str, int startIndex, int endIndex) {
+        if (str != null) {
+            char[] arr = str.toCharArray();
+            for (int i = startIndex, j = endIndex; i < j; i++, j--) {
+                char temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            return new String(arr);
+        }
         return null;
     }
 
+    //方法2
+    public String reverse2(String str, int startIndex, int endIndex) {
+        if (str != null) {
+
+            String reverStr = str.substring(0, startIndex);
+
+            for (int i = endIndex; i >= startIndex; i--) {
+                reverStr += str.charAt(i);
+            }
+
+            reverStr += str.substring(endIndex + 1);
+            return reverStr;
+        }
+        return null;
+    }
+    //方法三
+    public String reverse3(String str, int startIndex, int endIndex) {
+        if (str != null) {
+            //第一部分
+            StringBuilder builderStr = new StringBuilder(str.length());
+            builderStr.append(str.substring(0, startIndex));
+            //第二部分
+            for (int i = endIndex; i >= startIndex; i--) {
+                builderStr.append(str.charAt(i));
+            }
+            //第三部分
+            builderStr.append(str.substring(endIndex + 1));
+            return builderStr.toString();
+        }
+        return null;
+    }
+
+
+    @Test
+    public void testReverse() {
+        String str = "ab123456qwd";
+        System.out.println(str);
+        String reverseStr = reverse3(str, 2, 6);
+        System.out.println(reverseStr);
+    }
 
 }
