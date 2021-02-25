b32d9b589a26eb186d5cc6e8da91e12996d226a8
Justin.Zhang <justinzhangyf@gmail.com>
2021-02-25 15:56:46 +0800

    LocalDateTime等类的实例化及方法

--- day04/src/com/atguigu/Date/JDK8DateTimeTest.java
+++ day04/src/com/atguigu/Date/JDK8DateTimeTest.java
@@ -2,6 +2,9 @@ package com.atguigu.Date;
 
 import org.junit.Test;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
 
 /**
@@ -16,5 +19,56 @@ public class JDK8DateTimeTest {
         //偏移量
         Date date1 = new Date(2020 - 1900,9 - 1,8);
         System.out.println(date1);//Tue Sep 08 00:00:00 GMT+08:00 2020
+    }
+
+    /*
+    LocalDate、LocalTime、LocalDateTime 的使用
+    说明：
+        1.LocalDateTime相较于LocalDate、LocalTime，使用频率要高
+        2.类似于Calendar
+     */
+
+    @Test
+    public void test1(){
+        //now():获取当前的日期、时间、日期+时间
+        LocalDate localDate = LocalDate.now();
+        LocalTime localTime = LocalTime.now();
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        System.out.println(localDateTime);
+        System.out.println(localDate);
+        System.out.println(localTime);
+
+        //of():设置指定的年、月、日、时、分、秒。没有偏移量
+        LocalDateTime localDateTime1 = LocalDateTime.of(2020, 10, 6, 13, 43, 22);
+        System.out.println(localDateTime1);
+
+
+        //getXxx()：获取相关的属性
+        System.out.println(localDateTime.getDayOfMonth());
+        System.out.println(localDateTime.getDayOfWeek());
+        System.out.println(localDateTime.getMonth());
+        System.out.println(localDateTime.getMonthValue());
+        System.out.println(localDateTime.getMinute());
+
+
+        //体现不可变性
+        //withXxx():设置相关的属性
+        LocalDate localDate1 = localDate.withDayOfMonth(22);
+        System.out.println(localDate);
+        System.out.println(localDate1);
+
+
+        LocalDateTime localDateTime2 = localDateTime.withHour(4);
+        System.out.println(localDateTime);
+        System.out.println(localDateTime2);
+
+        //不可变性
+        LocalDateTime localDateTime3 = localDateTime.plusMonths(3);
+        System.out.println(localDateTime);
+        System.out.println(localDateTime3);
+
+        LocalDateTime localDateTime4 = localDateTime.minusDays(6);
+        System.out.println(localDateTime4);
     }
 }
