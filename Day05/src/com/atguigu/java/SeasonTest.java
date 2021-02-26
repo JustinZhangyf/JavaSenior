ca4765c859998ad7fa659d558a17a1c85013ab67
justinzhang <justinzhangyf@gmail.com>
2021-02-26 22:39:45 +0800

    自定义枚举类方法和过程

--- Day05/src/com/atguigu/java/SeasonTest.java
+++ Day05/src/com/atguigu/java/SeasonTest.java
@@ -6,5 +6,41 @@ package com.atguigu.java;
  * @date Created in 2/26/2021-3:06 PM
  */
 public class SeasonTest {
-        
+    public static void main(String[] args) {
+        Season autumn = Season.AUTUMN;
+        System.out.println(autumn);
+    }
+}
+
+class Season{
+    //1.声明Season对象的属性，private final修饰
+    private final String seasonName;
+    private final String seasonDesc;
+    //2.私有化构造器，并给对象属性赋值
+    private Season(String seasonName,String seasonDesc){
+        this.seasonName = seasonName;
+        this.seasonDesc = seasonDesc;
+    }
+    //3.提供当前枚举类的多个对象，public static final修饰
+    public static final Season SPRING = new Season("春天","春暖花开");
+    public static final Season SUMMER = new Season("夏天","夏日炎炎");
+    public static final Season AUTUMN = new Season("秋天","秋高气爽");
+    public static final Season WINTER = new Season("冬天","冰天雪地");
+    //其他诉求：获取枚举类对象的属性
+
+    public String getSeasonName() {
+        return seasonName;
+    }
+
+    public String getSeasonDesc() {
+        return seasonDesc;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Season{" +
+                "seasonName='" + seasonName + '\'' +
+                ", seasonDesc='" + seasonDesc + '\'' +
+                '}';
+    }
 }
